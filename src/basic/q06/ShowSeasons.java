91fef96af4d7e79b5e77e64498c13350d371b4cf
S Abe <177551857+SA-1826@users.noreply.github.com>
2025-08-10 19:09:16 +0900

    "finished q06"

---
+++ src/basic/q06/ShowSeasons.java
@@ -0,0 +1,15 @@
+package basic.q06;
+
+public class ShowSeasons {
+	public static void main(String[] args) {
+		var month = 13;
+		switch (month) {
+			case 1, 2, 12 -> System.out.println(month + "月は冬です");
+			case 3, 4, 5 -> System.out.println(month + "月は春です");
+			case 6, 7, 8 -> System.out.println(month + "月は夏です。");
+			case 9, 10, 11 -> System.out.println(month + "月は秋です");
+			default -> System.out.println(month + "月は存在しないです。");
+		}
+	}
+
+}
