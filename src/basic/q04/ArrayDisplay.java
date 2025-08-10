b6627bc26b095b3caa61344a434468f1cc96b8bd
S Abe <177551857+SA-1826@users.noreply.github.com>
2025-08-10 16:19:00 +0900

    "first commit (finished q04)"

---
+++ src/basic/q04/ArrayDisplay.java
@@ -0,0 +1,12 @@
+package basic.q04;
+
+public class ArrayDisplay {
+	public static void main(String[] args) {
+		String[] fruits = { "りんご", "ばなな", "みかん", "ぶどう", "すいか" };
+		for (int i = 0; i < fruits.length; i++) {
+			var fruit = fruits[i];
+			System.out.println(fruit);
+		}
+	}
+
+}
